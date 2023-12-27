33757f31a12a1e1d52029183949adb4d18d36803
Ahmadbinshoaib <ahmadshoaib1044@gmail.com>
2023-12-27 18:59:46 +0500

    Side-Nav Updated

--- TransgenderDosti/src/app/teacher-sidebar/teacher-sidebar.component.ts
+++ TransgenderDosti/src/app/teacher-sidebar/teacher-sidebar.component.ts
@@ -1,33 +1,46 @@
-import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router, Event as NavigationEvent } from '@angular/router';
+import { filter, take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-teacher-sidebar',
   templateUrl: './teacher-sidebar.component.html',
   styleUrls: ['./teacher-sidebar.component.css']
 })
-export class TeacherSidebarComponent {
+export class TeacherSidebarComponent implements OnInit {
   isMenuOpen = true;
   opened = true;
-  contentMargin = 195
+  contentMargin = 195;
+  selectedRoute = '';
+  isChevronDown = false;
 
-  constructor(private route: ActivatedRoute) {
-    this.route.url.subscribe(segments => {
-      console.log('Current URL Segments:', segments);
+  toggleChevron() {
+    this.isChevronDown = !this.isChevronDown;
+  }
+
+  constructor(private route: ActivatedRoute, private router: Router) { }
+
+  ngOnInit() {
+    // Navigate to teacher-course-dashboard initially
+    this.router.navigate(['/teacher-course-details/teacher-course-dashboard']);
+
+    // Subscribe to route changes
+    this.router.events.pipe(
+      filter((event) => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEvent) => {
+      const navigationEnd = event as NavigationEnd;
+      this.selectedRoute = navigationEnd.urlAfterRedirects;
+      console.log('Updated Route:', this.selectedRoute);
     });
   }
 
   onToolbarMenuToggle() {
     this.isMenuOpen = !this.isMenuOpen;
-    console.warn(this.isMenuOpen)
 
     if (!this.isMenuOpen) {
-      this.contentMargin = 70
+      this.contentMargin = 26;
+    } else {
+      this.contentMargin = 195;
     }
-    else {
-      this.contentMargin = 195
-    }
-
   }
-
 }
